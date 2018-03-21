9c051e6083889db987e2314c231329da06a7dd29
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2018-03-21 15:47:06 +0200

    Better hook

--- hook.php
+++ hook.php
@@ -1,25 +1,34 @@
 <?php
 $content = file_get_contents("php://input");
-$update = json_decode($content, true);
 
-if(isset($update['message'])) {
-    $message = $update['message'];
-    $message_id = $message['message_id'];
-    $chat_id = $message['chat']['id'];
-    $from_id = $message['from']['id'];
+$token = getenv('BOTTOKEN');
+echo $token;
 
-    $text = $message['text'];
+if($content) {
+    $update = json_decode($content, true);
 
-    $parameters = array(
-        'chat_id' => $chat_id,
-        'text' => "You wrote '{$text}'"
-    ));
+    if(isset($update['message'])) {
+        $message = $update['message'];
+        $message_id = $message['message_id'];
+        $chat_id = $message['chat']['id'];
+        $from_id = $message['from']['id'];
 
-    $token = getenv('BOTTOKEN');
+        $text = $message['text'];
 
-    $url = "https://api.telegram.org/bot{$token}/sendMessage";
-    $handle = curl_init($url);
-    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($handle, CURLOPT_POST, true);
-    $response = curl_exec($handle);
+        $parameters = array(
+            'chat_id' => $chat_id,
+            'text' => "You wrote '{$text}'"
+        ));
+
+        $url = "https://api.telegram.org/bot{$token}/sendMessage";
+        $query_string = http_build_query($parameters);
+        if(!empty($query_string)) {
+            $url .= '?' . $query_string;
+        }
+
+        $handle = curl_init($url);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handle, CURLOPT_POST, true);
+        $response = curl_exec($handle);
+    }
 }
